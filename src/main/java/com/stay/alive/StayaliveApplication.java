071e18cff97988aadb1695746a704b3cf8aade6b
Appledayz <44154226+Appledayz@users.noreply.github.com>
2018-12-04 13:58:24 +0900

    init

---
+++ src/main/java/com/stay/alive/StayaliveApplication.java
@@ -0,0 +1,12 @@
+package com.stay.alive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayaliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayaliveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stay/alive/StayaliveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stay.alive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StayaliveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
