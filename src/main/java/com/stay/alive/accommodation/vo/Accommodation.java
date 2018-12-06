4f6e5f7c979ad4e4eb318283c25506cec9d534d3
Administrator <Administrator@MSDN-SPECIAL>
2018-12-06 15:35:37 +0900

    [이영호] lombok 의존성, vo코드 추가
    1. lombok 의존성 추가
    2. 숙소, 업체 관련 vo작성

--- src/main/java/com/stay/alive/accommodation/vo/Accommodation.java
+++ src/main/java/com/stay/alive/accommodation/vo/Accommodation.java
@@ -1,5 +1,33 @@
- package com.stay.alive.accommodation.vo;
+package com.stay.alive.accommodation.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Accommodation {
-
+	private String memberId; //숙박 업체 회원 아이디
+	private int companyNo; //업체 등록 번호(FK)
+	private String companyName; //업체명
+	private int accommodationCategoryNo; //숙소 카테고리 번호(FK)
+	private String accommodationCategoryName; //숙소 카테고리 명
+	private String accommodationName; //숙소명
+	private int accommodationLatitude; //숙소 위도
+	private int accommodationHardness; //숙소 경도
+	private String addressDoNo; //도 번호(FK)
+	private String addressSiNo; //시 번호(FK)
+	private String addressGuNo; //구 번호(FK)
+	private String accommodationAddress; //숙소 주소
+	private int accommodationImageCount; //숙소 이미지 개수
+	private String memberBusinessNumber; //사업자 번호
+	private String accommodationConstructionDate; //건축일
+	private int accommodationScore; //평점
+	private int accommodationGuestroomTotal; //총 객실 수
+	private String accommodationPhone; //숙소 연락처
+	private String accommodationEmail; //숙소 이메일
+	private String accommodationDetail; //세부내용
+	private String accommodationRegisterDate; //숙소 등록일자
+	private String accommodationUpdateDate; //마지막 숙소 등록정보 수정일
 }
--- src/main/java/com/stay/alive/company/vo/Company.java
+++ src/main/java/com/stay/alive/company/vo/Company.java
@@ -1,5 +1,30 @@
 package com.stay.alive.company.vo;
 
-public class Company {
+import com.stay.alive.accommodation.vo.Accommodation;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Company {
+	private String companyName; //업체명
+	private String memberId; //숙박 업체 회원 아이디
+	private String ratingNo; //회원 등급 번호(FK)
+	private String ratingName; //등급명
+	private int companyVolume; //업체 거래량
+	private int memberOptionNo; //회원옵션번호(FK)
+	private String memberOptionName; //옵션명
+	private int registerSalespostTotal; //등록한 판매 게시물 수
+	private int registerAccommodationCount; //등록한 숙박시설 수
+	private int reverseauctionRegisterCount; //현재 역경매 입찰 현황(역경매 등록현황)
+	private int reverseauctionSuccessfulbidCount; //역경매 낙찰횟수
+	private int dutchauctionRegisterCount; //네덜란드식 경매 등록 현황
+	private int dutchauctionSalesCount; //네덜란드식 경매 통한 판매량
+	private int companyWishlistCount; //찜한 사람수
+	private String companyHomepage; //홈페이지
+	private String companyRegisterDate; //업체 등록일자
+	private String companyUpdateDate; //마지막 업체 등록정보 수정일
 }
