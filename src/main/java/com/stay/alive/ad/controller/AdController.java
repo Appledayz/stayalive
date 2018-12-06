a41e07b9cfec7f58635bc4e60067d28f9d96ce20
Administrator <Administrator@MSDN-SPECIAL>
2018-12-06 17:04:20 +0900

    [이찬우] 광고 컨트롤러/서비스 뼈대 구축중

--- src/main/java/com/stay/alive/ad/controller/AdController.java
+++ src/main/java/com/stay/alive/ad/controller/AdController.java
@@ -1,5 +1,26 @@
 package com.stay.alive.ad.controller;
 
-public class AdController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.stay.alive.ad.service.AdService;
+
+@Controller
+public class AdController {
+	@Autowired
+	private AdService adService;
+	
+	// 1. 광고 신청
+	
+	// 1.1 광고 등록 폼
+	
+	
+	
+	// 2. 광고 수정 폼
+	
+	// 2.1 광고 수정 액션
+	
+	// 3. 광고 삭제
+	
+	// 4. 광고 조회
 }
--- src/main/java/com/stay/alive/ad/service/AdService.java
+++ src/main/java/com/stay/alive/ad/service/AdService.java
@@ -1,5 +1,17 @@
 package com.stay.alive.ad.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.stay.alive.ad.mapper.AdMapper;
+
+@Service
+@Transactional
 public class AdService {
+	@Autowired
+	private AdMapper adMapper;
+	
+	// 광고 목록 조회
 
 }
