c3fa2042325fad048418527e43ee8b07c1a6c680
Administrator <Administrator@MSDN-SPECIAL>
2018-12-14 16:04:40 +0900

    [강승우] admin 페이지 템플릿 추가

---
+++ src/main/java/com/stay/alive/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.stay.alive.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+	@GetMapping("admin")
+	public String admin() {
+		return "admin/admin";
+	}
+}
