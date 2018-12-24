ff5dee47fb79eaafbd71c44b128fc896ad20815a
Administrator <Administrator@MSDN-SPECIAL>
2018-12-24 13:47:44 +0900

    [강승우] 숙소 view 수정 및 header 수정

--- src/main/java/com/stay/alive/board/controller/BoardController.java
+++ src/main/java/com/stay/alive/board/controller/BoardController.java
@@ -1,5 +1,15 @@
 package com.stay.alive.board.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("board")
+public class BoardController {
+	
+	@GetMapping("free")
+	public String board() {
+		return "board/freeBoard";
+	}
 }
