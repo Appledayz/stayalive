2846c88e8ce17432aa73875a8ef3296b79ebd305
Administrator <Administrator@MSDN-SPECIAL>
2019-01-03 09:20:39 +0900

    [이찬우] 광고 삭제 , 로그인html 수정

--- src/main/java/com/stay/alive/board/service/BoardService.java
+++ src/main/java/com/stay/alive/board/service/BoardService.java
@@ -31,13 +31,7 @@ public class BoardService {
 	public void addBoardMember(BoardMember boardMember, String path) {		
 		System.out.println("게시판 등록");
 		boardMember.setBoardCategoryNo(1);
-		boardMember.setBoardCategoryName("자유게시판");
-		
-		/*int groupNo = boardMember.getGroupNo();
-		String groupName = boardMapper.selectGroupName(groupNo);
-		System.out.println(groupName);
-		boardMember.setGroupName(groupName);*/
-		
+		boardMember.setBoardCategoryName("자유게시판");		
 		boardMapper.insertBoard(boardMember);
 	}
 	//게시판 수정
